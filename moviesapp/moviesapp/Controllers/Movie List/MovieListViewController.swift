4bd03573eb9e7c04bc307bc37362f46a9186d5be
Wei Qian <wqyap762@hotmail.com>
2018-08-31 01:00:37 +0800

    Updated to display image in collection view in Movie List View

---
+++ moviesapp/moviesapp/Controllers/Movie List/MovieListViewController.swift
@@ -0,0 +1,47 @@
+//
+//  MovieListViewController.swift
+//  MoviesApp
+//
+//  Created by Wei Qian on 31/08/2018.
+//  Copyright © 2018 Yap Wei Qian. All rights reserved.
+//
+
+import UIKit
+
+class MovieListViewController: UIViewController {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        collectionView.register(UINib(nibName: "MovieCell", bundle: nil), forCellWithReuseIdentifier: "MovieCell")
+        collectionView.dataSource = self
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.navigationController?.isNavigationBarHidden = false
+        self.navigationController?.navigationBar.prefersLargeTitles = true
+        self.title = "Movies"
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
+extension MovieListViewController: UICollectionViewDataSource {
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MovieCell", for: indexPath) as! MovieCell
+        return cell
+    }
+    
+}
---
+++ moviesapp/moviesapp/Controllers/Movie List/Views/MovieCell.swift
@@ -0,0 +1,20 @@
+//
+//  MovieCollectionViewCell.swift
+//  MoviesApp
+//
+//  Created by Wei Qian on 31/08/2018.
+//  Copyright © 2018 Yap Wei Qian. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UICollectionViewCell {
+
+    @IBOutlet weak var posterImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
