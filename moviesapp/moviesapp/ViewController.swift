741223ccc82682445432e1c9f893b62f78ab46da
Wei Qian <wqyap762@hotmail.com>
2018-08-30 19:03:21 +0800

    Initialized project

---
+++ moviesapp/moviesapp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MoviesApp
+//
+//  Created by Wei Qian on 31/08/2018.
+//  Copyright © 2018 Yap Wei Qian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
