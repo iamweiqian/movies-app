c15b038e1735f2bafcc7aa8879552d27ac03f51f
Wei Qian <wqyap762@hotmail.com>
2018-08-31 01:15:39 +0800

    Updated to display items in grid layout

--- moviesapp/moviesapp/Controllers/Movie List/MovieListViewController.swift
+++ moviesapp/moviesapp/Controllers/Movie List/MovieListViewController.swift
@@ -12,13 +12,14 @@ class MovieListViewController: UIViewController {
 
     @IBOutlet weak var collectionView: UICollectionView!
     
-    let images = [#imageLiteral(resourceName: "Polar Bear Pillow Case"), #imageLiteral(resourceName: "Polar Bear Pillow Case"), #imageLiteral(resourceName: "Polar Bear Pillow Case"), #imageLiteral(resourceName: "Polar Bear Pillow Case"), #imageLiteral(resourceName: "Polar Bear Pillow Case")]
+    let images = [#imageLiteral(resourceName: "Polar Bear Pillow Case"), #imageLiteral(resourceName: "programming"), #imageLiteral(resourceName: "Polar Bear Pillow Case"), #imageLiteral(resourceName: "programming"), #imageLiteral(resourceName: "programming"), #imageLiteral(resourceName: "Polar Bear Pillow Case"), #imageLiteral(resourceName: "programming")]
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         collectionView.register(UINib(nibName: "MovieCell", bundle: nil), forCellWithReuseIdentifier: "MovieCell")
         collectionView.dataSource = self
+        collectionView.delegate = self
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -33,6 +34,19 @@ class MovieListViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+}
+
+extension MovieListViewController: UICollectionViewDelegateFlowLayout {
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let numberOfColumns: CGFloat = 2
+        let width = collectionView.frame.size.width
+        let xInsets: CGFloat = 10
+        let cellSpacing: CGFloat = 5
+        
+        return CGSize(width: (width / numberOfColumns) - (xInsets + cellSpacing), height: (width / numberOfColumns) - (xInsets + cellSpacing))
+    }
+    
 }
 
 extension MovieListViewController: UICollectionViewDataSource {
